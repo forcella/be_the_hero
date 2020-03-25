265ab14ef887c1b70afda4132ab9239db9a288de
Lucas Guerra <lucas.guerra@matera.com>
2020-03-25 19:48:46 -0300

    Added basic resources for front end and some fixes to backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -3,25 +3,33 @@ const crypto = require(`crypto`);
 
 
 module.exports = {
-    async index(_request,response) {
+    async index(_request, response) {
         const ongs = await connection(`ongs`).select(`*`);
         return response.json(ongs)
     },
 
 
-    async create(request,response){
-        const {name,email,whatsapp,city,uf} = request.body;
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
         const id = crypto.randomBytes(4).toString(`HEX`);
-    
-        await connection(`ongs`).insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf
-        })
 
-        return response.json({id});
+        try {
+            await connection(`ongs`).insert({
+                id,
+                name,
+                email,
+                whatsapp,
+                city,
+                uf
+            })
+        } catch (error) {
+            const err = String(error)
+            const UNIQUE_CONSTRAINT = `ongs.`;
+            const field = err.substr(err.lastIndexOf(UNIQUE_CONSTRAINT) + UNIQUE_CONSTRAINT.length)
+            return response.status(400).json({ field })
+        }
+
+
+        return response.json({ id });
     }
 }
--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,13 +2,13 @@ const connection = require(`../database/connection`)
 
 
 module.exports = {
-    async index(_request,response) {
-        const ong_id = request.headers.authorization;
+    async index(request, response) {
+        const ong_id = request.headers.authorization || null;
 
         const incidents = await connection(`incidents`)
-            .where(`ong_id`,ong_id)
+            .where(`ong_id`, ong_id)
             .select(`*`);
-        
+
         return response.json(incidents)
     },
 }
